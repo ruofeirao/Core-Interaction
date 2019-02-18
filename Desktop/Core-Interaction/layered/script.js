60857a99d262c518c4921a54110ab9584681ea59
Ruofei Rao <raor390@newschool.edu>
2019-02-18 01:01:26 -0500

    hi

---
+++ Desktop/Core-Interaction/layered/script.js
@@ -0,0 +1,16 @@
+var controller = new ScrollMagic.Controller();
+var scene = new ScrollMagic.Scene({triggerElement: "#trigger"})
+                .setVelocity("#january", {opacity: 0}, {duration: 100})
+                .addTo(controller);
+
+                var controller = new ScrollMagic.Controller();
+
+var scene = new ScrollMagic.Scene({
+                    triggerElement: '#pinned-trigger1', // starting scene, when reaching this element
+                    duration: 400 // pin the element for a total of 400px
+                  })
+                  .setPin('#pinned-element1'); // the element we want to pin
+                  
+                  // Add Scene to ScrollMagic Controller
+                  controller.addScene(scene);
+
